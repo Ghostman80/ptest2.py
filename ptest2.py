b570838e500e3bf962310466086618021f49e24d
Jared Johnson <60166120+Ghostman80@users.noreply.github.com>
2022-01-11 10:52:43 -0700

    Add files via upload

---
+++ ptest2.py
@@ -0,0 +1,32 @@
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = float(num1) + float(num2)
+
+print(result)
+
+input("Thanks for playing!")
+
+x = input("Enter a number: ")
+y = input("Enter another number: ")
+z = input("Enter a third number: ")
+result = float(x) + float(y) + float(z)
+
+print(result)
+
+input("Math is fun!")
+
+name = input("Enter your name: ")
+occupation = input("Enter your occupation: ")
+
+print("Hello " + name + "! You are a " + occupation)
+print("Welcome to produce counter!")
+
+tomatos = input("Tomatos: ")
+apples = input("Apples: ")
+oranges = input("Oranges: ")
+result = float(tomatos) + float(apples) + float(oranges)
+
+print(result) #Total amount of produce.
+
+input("Total amount of produce: ")
+input("Thank you! ")
